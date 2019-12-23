590c8b38bb7b5f1f17dcb5f945b02fcc2778e8ee
Darren Swan <thegundamguru@outlook.com>
2019-12-22 21:54:30 -0600

    Refactor out device base and namespaces

--- src/DeviceBase/Abstractions/IDeviceAction.cs
+++ src/DeviceBase/Abstractions/IDeviceAction.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
 
-namespace BecauseImClever.DeviceBase.Abstractions
+namespace BecauseImClever.HomeAutomation.DeviceBase.Abstractions
 {
-    public interface IDeviceAction
-    {
-        void DoSomething();
-    }
+	public interface IDeviceAction
+	{
+		public void Invoke();
+	}
+	public interface IDeviceAction<T> : IDeviceAction
+	{
+		public Task<T> GetValueAsync();
+		public T GetValue();
+
+		public Task<bool> SetValueAsync(T value);
+		public bool SetValue(T value);
+		public void Invoke(T value);
+
+	}
 }
--- src/DeviceBase/Device.cs
+++ src/DeviceBase/Device.cs
@@ -1,15 +1,14 @@
-﻿using BecauseImClever.DeviceBase.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BecauseImClever.DeviceBase
+﻿
+namespace BecauseImClever.HomeAutomation.DeviceBase
 {
-    public abstract class Device
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public IEnumerable<IDeviceAction> Actions { get; internal set; }
+	using Abstractions;
+	using System;
+	using System.Collections.Generic;
 
-    }
+	public abstract class Device
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public IEnumerable<IDeviceAction> Actions { get; internal set; }
+	}
 }
