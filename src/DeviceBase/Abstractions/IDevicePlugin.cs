a03b6478f81c4a8d7dbe34ec98248c014e5bf9de
Darren Swan <fortinbra@becauseimclever.com>
2020-02-08 18:54:36 -0600

    adding idevice plugin

---
+++ src/DeviceBase/Abstractions/IDevicePlugin.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BecauseImClever.HomeAutomation.DeviceBase.Abstractions
+{
+	public interface IDevicePlugin
+	{
+		public string Name { get; }
+		public string Description { get; }
+		public IEnumerable<IDeviceAction> DeviceActions { get; }
+		public void RegisterDependencies(IServiceCollection services);
+	}
+}
