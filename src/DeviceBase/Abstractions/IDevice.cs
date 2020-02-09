887690a7d12f895edcd4543e03e28a6c9df5ffe0
Darren Swan <fortinbra@becauseimclever.com>
2020-02-08 18:58:43 -0600

    adding idevice, switching from abstract to interface

---
+++ src/DeviceBase/Abstractions/IDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BecauseImClever.HomeAutomation.DeviceBase.Abstractions
+{
+    public interface IDevice
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<IDeviceAction> Actions { get; internal set; }
+
+    }
+}
--- src/DeviceBase/Abstractions/IDevicePlugin.cs
+++ src/DeviceBase/Abstractions/IDevicePlugin.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BecauseImClever.HomeAutomation.DeviceBase.Abstractions
 {
-	public interface IDevicePlugin
-	{
-		public string Name { get; }
-		public string Description { get; }
-		public IEnumerable<IDeviceAction> DeviceActions { get; }
-		public void RegisterDependencies(IServiceCollection services);
-	}
+    public interface IDevicePlugin
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public IEnumerable<IDeviceAction> DeviceActions { get; }
+        public void RegisterDependencies(IServiceCollection services);
+    }
 }
